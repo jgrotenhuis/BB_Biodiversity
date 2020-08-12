d9156da7248f6f8f6c90a37422a8329fc2f66ab3
Jonny Grotenhuis <grotenhuis@google.com>
2020-08-11 17:51:39 -0700

    Challenge upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# BB_Biodiversity
+# BB_Biodiversity Overview
+
+The goal of this module and challenge was to build an interactive and customizable dashboard for volunteers to login and see results.  
+
+These results will help researchers identify which bacteria types they should buy for synthetic beef production.  Roza's credibility will definitely jump from this work!
+
+This was a fun challenge and I enjoyed creating various types of plots to achieve cool visualizations for Roza.
--- plots.js
+++ plots.js
@@ -4,14 +4,17 @@ function init() {
   d3.json("samples.json").then((data) => {
     console.log(data);
     var sampleNames = data.names;
+    var sample = sampleNames[0];
     sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
         .property("value", sample);
-    });
-})}
 
+    });
+    buildMetadata(sample);
+    buildCharts(sample);
+})}
 init();
 
 function buildMetadata(sample) {
@@ -22,13 +25,64 @@ function buildMetadata(sample) {
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
-    PANEL.append("h6").text("ID: " + result.id);
-    PANEL.append("h7").text("ETHNICITY: " + result.ethnicity);
-    PANEL.append("h8").text("GENDER: " + result.gender);
-    PANEL.append("h9").text("AGE: " + result.age);
-    PANEL.append("h10").text("LOCATION: " + result.location);
-    PANEL.append("h11").text("BBTYPE: " + result.bbtype);
-    PANEL.append("h12").text("WFREQ: " + result.wfreq);
+    PANEL.append("h6").text("ID: " + result.id + "\n");
+    PANEL.append("h6").text("ETHNICITY: " + result.ethnicity + "\n");
+    PANEL.append("h6").text("GENDER: " + result.gender + "\n");
+    PANEL.append("h6").text("AGE: " + result.age + "\n");
+    PANEL.append("h6").text("LOCATION: " + result.location + "\n");
+    PANEL.append("h6").text("BBTYPE: " + result.bbtype + "\n");
+    PANEL.append("h6").text("WFREQ: " + result.wfreq + "\n");
+  });
+}
+
+function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var otu_ids = result.otu_ids
+
+    var sample_values = result.sample_values
+    var top_10 = sample_values.slice(0, 10)
+    var otu_labels = result.otu_labels
+    
+    var metadata = data.metadata;
+
+    var bar_trace = [ {
+      x: top_10,
+      y: result,
+      type: "bar",
+      orientation: 'h',
+      text: otu_labels
+    } ];
+    var bar_layout = {
+      yaxis: {autorange:'reversed'}
+    };
+    Plotly.newPlot("bar", bar_trace, bar_layout);
+
+    // var gauge_trace = {
+    //   value: [wfreq],
+    //   title: { text: "Belly Button Washing Frequency\n Scrubs Per Week"},
+    //   type: "indicator",
+    //   showarrow: "False"
+    // };
+    // Plotly.newPlot("gauge", gauge_trace);
+
+    var bubble_trace = {
+      x: otu_ids,
+      y: sample_values,
+      mode: "markers",
+      text: otu_labels,
+      marker: {
+        size: sample_values,
+        color: otu_ids
+      }
+    };
+
+    var bubble_data = [bubble_trace];
+
+    Plotly.newPlot("markers", bubble_data);
+
   });
 }
 
